deff2c7958d78887a752b691d238bb1a07cb5c5c
Clyde Germond <clyde.germond@gmail.com>
2021-04-30 09:40:41 +0200

    initial commit

---
+++ src/main/java/test/clyde/gradle/GradleTestApplication.java
@@ -0,0 +1,13 @@
+package test.clyde.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/clyde/gradle/GradleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.clyde.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
